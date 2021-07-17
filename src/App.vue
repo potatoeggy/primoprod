c710790dca591d83532c37173bc5810cdeeba127
eggy <eggyrules@gmail.com>
2021-07-16 22:29:37 -0400

    Add wish buttons

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,25 @@
 <template>
+  <div id="header">
+    <p class="white">Wish</p>
+  </div>
+  <div id="banner">
+  </div>
+  <div id="bottom-row">
+    <wish-button wishes="1"></wish-button>
+    <wish-button wishes="10"></wish-button>
+  </div>
   <media-test></media-test>
 </template>
 
 <script lang="ts">
 import { Options, Vue } from "vue-class-component";
 import MediaTest from "./components/MediaTest.vue";
+import WishButton from "./components/WishButton.vue";
 
 @Options({
   components: {
     MediaTest,
+    WishButton,
   },
 })
 export default class App extends Vue {}
@@ -19,6 +30,7 @@ export default class App extends Vue {}
   font-family: "Genshin";
   src: local("Genshin"), url(./assets/fonts/Genshin.ttf) format("truetype");
 }
+
 #app {
   font-family: Genshin, Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -29,6 +41,11 @@ export default class App extends Vue {}
       fixed,
     black;
   cursor: url("./assets/images/cursor.png"), auto;
+  min-height: max-content;
+}
+
+.white {
+  color: white;
 }
 
 /* reset css TODO: the wish bg is animated :/ */
