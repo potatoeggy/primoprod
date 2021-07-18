7ebc24dc14a6e7e210ddb108e43d76426b4d325b
eggy <eggyrules@gmail.com>
2021-07-17 21:21:46 -0400

    Add plus sign

---
+++ src/components/PlusButton.vue
@@ -0,0 +1,27 @@
+<template>
+  <button type="button" class="plus-button"><p>+</p></button>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+export default defineComponent({});
+</script>
+
+<style scoped>
+.plus-button {
+  display: relative;
+  font-family: Genshin, Arial, Helvetica, sans-serif;
+  border: none;
+  width: 1.75rem;
+  height: 1.75rem;
+  border-radius: 50%;
+  background-color: #ece5d8;
+  font-size: 22pt;
+  text-align: center;
+
+}
+
+.plus-button > p {
+  margin-top: -2px;
+}
+</style>
