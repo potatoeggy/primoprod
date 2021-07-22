22730adfc3af5f0bdd555ce76196ea96654574e3
eggy <eggyrules@gmail.com>
2021-07-22 18:31:42 -0400

    Switch to standard vue from class component

--- src/App.vue
+++ src/App.vue
@@ -35,95 +35,96 @@
 </template>
 
 <script lang="ts">
-import { Options, Vue } from "vue-class-component";
-import { defineAsyncComponent } from "vue"; // TODO: migrate entirely to normal vue
+import { defineAsyncComponent, defineComponent } from "vue";
 import WishBanners from "./components/WishBanners.vue";
 import FatePurchaseDialog from "./components/FatePurchaseDialog.vue";
-const VideoPlayer = defineAsyncComponent(
-  () => import("./components/VideoPlayer.vue")
-);
 
 // gacha
 import Gacha from "./banners/Gacha";
 import standardDrops from "./banners/wanderlust-invocation.json";
 
-@Options({
+export default defineComponent({
   components: {
     WishBanners,
     FatePurchaseDialog,
-    VideoPlayer,
+    VideoPlayer: defineAsyncComponent(
+      () => import("./components/VideoPlayer.vue")
+    ),
   },
-})
-export default class App extends Vue {
-  // save vars
-  // TODO: implement localStorage json storage
-  fates = 3;
-  primos = 5337;
-  starglitter = 4;
-  stardust = 700;
-  standardBanner = new Gacha(standardDrops);
+  data() {
+    return {
+      // storage vars
+      fates: 3,
+      primos: 5337,
+      starglitter: 4,
+      stardust: 700,
+      standardBanner: new Gacha(standardDrops),
+      // state vars
+      checkPullDialog: false,
+      pullNumber: 1,
+      screen: "wish-banner",
+    };
+  },
+  methods: {
+    wish(pulls: number): void {
+      this.pullNumber = pulls;
+      if (pulls <= this.fates) {
+        this.goWish();
+      } else {
+        this.checkPullDialog = true;
+      }
+    },
 
-  // state vars
-  checkPullDialog = false;
-  pullNumber = 1;
+    goWish(): void {
+      this.fates -= this.pullNumber;
+      this.checkPullDialog = false;
+      this.screen = "video-player";
+      // TODO: set rarity variable here
+      // also calc current pull here
+      // note our safety is guaranteed because
+      // the gacha class automatically stores on roll
+      // shit now we have to store primos/fates *separately*??
+    },
 
-  // the element names of a top-level element (like video-player)
-  // don't put overlays here!
-  screen = "wish-banner";
+    cancelWish(): void {
+      this.checkPullDialog = false;
+    },
 
-  mounted(): void {
+    // hacky :/ but the audio element is unfortunately unmounted
+    // before the audio fully plays
+    exitConfirmCancelDialog(fn: () => void, fatesNeeded: number): void {
+      (this.$refs.audioExitDialog as HTMLAudioElement).play();
+      if (fatesNeeded) {
+        this.primos -= fatesNeeded * 160;
+        this.fates += fatesNeeded;
+      }
+      fn();
+    },
+
+    showResults(): void {
+      this.screen = "wish-banner";
+      // TODO: state variable for current pull?
+    },
+
+    showResultsEnd(): void {
+      this.screen = "wish-banner";
+      // TODO: yikes you have to check if it's 10 or not since i
+      // think they have different uis
+    },
+  },
+  computed: {
+    fatesToPurchase(): number {
+      return this.pullNumber - this.fates;
+    },
+    pullRarity(): number {
+      return 4;
+    },
+  },
+  mounted() {
     const bgm: HTMLAudioElement = this.$refs.audioBgm as HTMLAudioElement;
     bgm.volume = 0.1;
-  }
-
-  wish(pulls: number): void {
-    this.pullNumber = pulls;
-    if (pulls <= this.fates) {
-      this.goWish();
-    } else {
-      this.checkPullDialog = true;
-    }
-  }
-
-  goWish(): void {
-    this.fates -= this.pullNumber;
-    this.checkPullDialog = false;
-    this.screen = "video-player";
-  }
-
-  cancelWish(): void {
-    this.checkPullDialog = false;
-  }
-
-  // hacky :/ but the audio element is unfortunately unmounted
-  // before the audio fully plays
-  exitConfirmCancelDialog(fn: () => void, fatesNeeded: number): void {
-    (this.$refs.audioExitDialog as HTMLAudioElement).play();
-    if (fatesNeeded) {
-      this.primos -= fatesNeeded * 160;
-      this.fates += fatesNeeded;
-    }
-    fn();
-  }
-
-  showResults(): void {
-    this.screen = "wish-banner";
-  }
-
-  showResultsEnd(): void {
-    this.screen = "wish-banner";
-  }
-
-  get fatesToPurchase(): number {
-    return this.pullNumber - this.fates;
-  }
-
-  get pullRarity(): number {
-    // TODO: make a pull handler object handling pity and all that then
-    // pass the function directly to videoplayer
-    return 4;
-  }
-}
+  },
+});
 </script>
 
 <style>
