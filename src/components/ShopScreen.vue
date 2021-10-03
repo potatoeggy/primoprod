ef11e6b98795f09bb1569334b1e929184c97720e
eggy <eggyrules@gmail.com>
2021-10-03 15:17:08 -0400

    shop: Add initial header
    Add relatively accurate GemCounters to the header bar. Also the first bit to properly use box-sizing: border-box.

--- src/components/ShopScreen.vue
+++ src/components/ShopScreen.vue
@@ -1,17 +1,54 @@
 <template>
   <div class="main">
-    {{ inventory.primos }}
+    <div class="header">
+      <close-button @clicked="exit"></close-button>
+      <div style="margin-right: 3rem">
+        <gem-counter
+          icon="primogem.png"
+          :text="inventory.primos"
+          plusSign
+          outline
+          nobackground
+        >
+        </gem-counter>
+      </div>
+      <gem-counter
+        icon="starglitter.png"
+        :text="inventory.starglitter"
+        outline
+        nobackground
+      >
+      </gem-counter>
+      <gem-counter
+        icon="stardust.png"
+        :text="inventory.stardust"
+        outline
+        nobackground
+      ></gem-counter>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
 import Inventory from "@/banners/Inventory";
 import { defineComponent } from "vue";
+import GemCounter from "./GemCounter.vue";
+import CloseButton from "./CloseButton.vue";
+
+// import a shop json
+
 export default defineComponent({
+  components: { GemCounter, CloseButton },
   props: {
     inventory: {
       type: Inventory,
       required: true,
+    },
+  },
+  methods: {
+    exit() {
+      // TODO: exit animation
+      this.$emit("exit");
     },
   },
   emits: ["exit"],
@@ -19,6 +56,14 @@ export default defineComponent({
 </script>
 
 <style scoped>
+.header {
+  display: flex;
+  flex-direction: row-reverse;
+  align-items: center;
+  width: 100%;
+  margin-right: 3rem;
+}
+
 /* TODO: re-use css via components this is super inefficient
  * right now since it's copied from QuestScreen
  */
@@ -33,6 +78,8 @@ export default defineComponent({
   align-items: center;
   z-index: 1;
   animation: main-entry 0.1s ease-in 0s forwards;
+  padding: 1rem;
+  box-sizing: border-box;
 }
 
 .exit-main {
@@ -45,7 +92,7 @@ export default defineComponent({
     backdrop-filter: none;
   }
   to {
-    background: linear-gradient(to bottom, #383d53, transparent);
+    background: linear-gradient(to bottom, #2c3047, transparent);
     backdrop-filter: blur(1.5rem);
   }
 }
@@ -56,7 +103,7 @@ export default defineComponent({
     backdrop-filter: none;
   }
   to {
-    background: linear-gradient(to bottom, #383d53, transparent);
+    background: linear-gradient(to bottom, #2c3047, transparent);
     backdrop-filter: blur(1rem);
   }
 }
