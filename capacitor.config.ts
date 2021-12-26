d4979946ea1d78db77ea475e5d276f90f56de923
eggy <eggyrules@gmail.com>
2021-12-26 18:53:41 -0500

    Add Android build via Capacitor

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'tk.eggworld.primoprod',
+  appName: 'primoprod',
+  webDir: 'dist',
+  bundledWebRuntime: false
+};
+
+export default config;
