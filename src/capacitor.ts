8a594fc462a41ac10b186074540547b1fde9310d
eggy <danielchen04@hotmail.ca>
2023-10-10 17:07:33 -0400

    chore: fix dev runtime errors

--- src/capacitor.ts
+++ src/capacitor.ts
@@ -1,4 +1,6 @@
 import { StatusBar } from "@capacitor/status-bar";
 
-StatusBar.setOverlaysWebView({ overlay: true });
-setInterval(() => StatusBar.hide(), 1000);
+StatusBar.setOverlaysWebView({ overlay: true }).catch(() => null);
+setInterval(() => {
+  StatusBar.hide().catch(() => null);
+}, 1000);
